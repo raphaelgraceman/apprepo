e3cdc1bbc9c9cd336dd73ae0cab52875dbad8d95
raphaelgraceman <raphaelgraceman@gmail.com>
2024-10-20 07:02:59 +0000

    Reviewed Hashed Password

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -30,13 +30,25 @@ async function registerAccount(req, res) {
   } = req.body;
 
   // Hash the password before storing
-
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
 
   const regResult = await accountModel.accountRegister(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
+
   );
   if (regResult) {
     req.flash(
